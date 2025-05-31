9e3c3a6586a713a07cd927edee2fb3a6b5066568
Tarun Singh <newtarunsh@gmail.com>
2025-05-31 06:07:05 +0000

    set the basic routing for the app

--- my-vue-app/src/components/Body.jsx
+++ my-vue-app/src/components/Body.jsx
@@ -1,15 +1,14 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-
+import { Outlet } from "react-router-dom";
 const Body = () => {
   return (
     <>
       <Navbar />
+      <Outlet />
       <Footer />
-
     </>
   );
 };
-
 
 export default Body;
--- my-vue-app/src/main.jsx
+++ my-vue-app/src/main.jsx
@@ -1,11 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider ls} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Body from "./components/Body.jsx";
+import Login from "./components/Body-Child/login.jsx";
+import About from "./components/Body-Child/about.jsx";
+import ErrorP from "./error.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+    errorElement: <ErrorP />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <RouterProvider router={router}></RouterProvider>
 );
