96a916954c17ab411639cceca874b67af75bf013
TarPrax <newtarunsh@gmail.com>
2025-07-04 15:20:14 +0530

    added code correction

--- src/components/Body-Child/UserEdit.jsx
+++ src/components/Body-Child/UserEdit.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
-import { endpoint } from "../endpoint"; // Adjust the import path as necessary
+import { endpoint } from "../../endpoint"; // Adjust the import path as necessary
 
 const UserEdit = () => {
 const user = useSelector((state) => state.user);
